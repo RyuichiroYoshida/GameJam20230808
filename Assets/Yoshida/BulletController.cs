04edf5ad3f3773cfe8925ee1d2f207a788233183
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-08-08 13:05:50 +0900

    [add] Bullet / Player

---
+++ Assets/Yoshida/BulletController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    [SerializeField] float _bulletDamage = 1;
+    [SerializeField] float _buletSpeed = 1;
+
+    Rigidbody2D _rb;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _rb.AddForce(Vector2.up * _buletSpeed, ForceMode2D.Impulse);
+        Destroy(this.gameObject, 1);    
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+           
+    }
+}
---
+++ Assets/Yoshida/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float _moveSpeed = 5;
+    [SerializeField] float _shotDelay = 0.5f;
+    [SerializeField] GameObject _bulletPrefab;
+
+    Rigidbody2D _rb;
+
+    float _xInput = 0;
+
+    public float XInput => _xInput;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(ShootDelay());
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(this);
+        }
+    }
+
+    void Move()
+    {
+        _xInput = Input.GetAxisRaw("Horizontal");
+        _rb.velocity = new Vector2(_moveSpeed * _xInput, _rb.velocity.y);
+    }
+
+    IEnumerator ShootDelay()
+    {
+        Instantiate(_bulletPrefab, new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + 0.8f), Quaternion.identity);
+        yield return new WaitForSeconds(_shotDelay);
+        StartCoroutine(ShootDelay());
+    }
+}
